f779970ae62db10882b2e9d7df30bad9c270b92e
zerovirus123 <briansia93@gmail.com>
2025-05-12 20:55:33 +0800

    remove commented code. format function call

--- asteroid.py
+++ asteroid.py
@@ -3,15 +3,14 @@ from circleshape import CircleShape
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        # if hasattr(self, "containers"):
-        #     super().__init__(self.containers)
-        # else:
-        #     super().__init__()
-
         Asteroid.containers = None
 
     def draw(self, screen, position, radius, width=2):
-        pygame.draw.circle(screen, color="white", center=position, radius=radius, width=width)
+        pygame.draw.circle(screen,
+                           color="white",
+                           center=position,
+                           radius=radius,
+                           width=width)
 
     def update(self, dt):
         self.position = self.velocity * dt
